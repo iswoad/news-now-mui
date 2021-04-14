a00b4a8a43e1a52efa1f8b6010a3621275cdc04d
iswoad <iswoadansary@gmail.com>
2021-04-14 17:01:14 +0600

    practiced use of material ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import News from './components/News/News';
+import {Button} from '@material-ui/core';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  useEffect(()=> {
+    const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=6f96212fc1704577926eb73a3b510f1e';
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setArticles(data.articles))
+  },0)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button variant="contained" color="secondary">
+        Secondary
+      </Button>
+      {
+        articles.map(article => <News article = {article}></News>)
+      }
     </div>
   );
 }
